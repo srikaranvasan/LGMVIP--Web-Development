feb514e4c38099613ffac198a2f8bfbd54350600
srikaranvasan <56198679+srikaranvasan@users.noreply.github.com>
2021-08-19 13:44:22 +0530

    Update script.js

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,45 +1,17 @@
-// const swiper = new Swiper('.swiper-container', {
-//     // Optional parameters
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-  
-//     // And if we need scrollbar
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//       draggable: true,
-//     },
-//     autoplay: {
-//         delay: 5000,
-//       }, 
-//   });
-
-// const togglebuton = document.getElementById('menu-bars')[0]
-// const navbarlinks = document.getElementById('nav-links')[0]
- 
-//  if(togglebuton){
-//     togglebuton.addEventListener('click',() =>{
-//         navbarlinks.classList.toggle('active')
-//     })
-//  }
-
+// Navbar
 let togglebuton = document.querySelector('#menu-bars');
 let navbarlinks = document.querySelector('#nav-links');
 togglebuton.onclick = () =>{
+
     navbarlinks.classList.toggle('active');
 }
 
+// Video slider
 function videoUrl(hmmmmmm){
     document.getElementById("slider").src = hmmmmmm;
 }
 
+// image slider
 function imgUrl(hmmmmmm){
     document.getElementById("img-slider").src = hmmmmmm;
 }
